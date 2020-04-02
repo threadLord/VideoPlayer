7c33d932e6b3f233d3d866cbcd869e9eaaa23a56
mare25 <markomutavdzic@gmail.com>
2020-04-02 09:08:34 +0200

    Initial Commit

---
+++ VideoPlayerLiveStream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerLiveStream
+//
+//  Created by OSX on 02/04/2020.
+//  Copyright © 2020 MarkoMutavdzic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
